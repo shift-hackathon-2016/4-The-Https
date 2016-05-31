8d69e2e2500fb78ac288bd685fdcbddcc17999ec
Mario <brizic.mario@gmail.com>
2016-05-31 16:56:54 +0200

    Added modal infrastructure

--- Web/Sonar.Web/Sonar.Web/AngularApp/Pages/Main/MainController.js
+++ Web/Sonar.Web/Sonar.Web/AngularApp/Pages/Main/MainController.js
@@ -1,5 +1,11 @@
 ﻿MainController.$inject = ["$scope", "$http"];
 function MainController($scope, $http) {
+
+    $scope.showModal = false;
+    $scope.toggleModal = function () {
+        $scope.showModal = !$scope.showModal;
+    };
+
     $scope.mockEvents = [
         { id: 1, title: 'First event', lat: -34.397, long: 150.644 },
         { id: 1, title: 'wfsg event', lat: -34.297, long: 150.644 },
