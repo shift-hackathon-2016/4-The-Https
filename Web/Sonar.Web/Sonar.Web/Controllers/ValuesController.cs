80c49d640862ae09ea5d484bd1ea1eadfc5a526b
zarko brkic <mceternal105@hotmail.com>
2016-05-31 14:23:59 +0200

    Fixed db error

--- Web/Sonar.Web/Sonar.Web/Controllers/ValuesController.cs
+++ Web/Sonar.Web/Sonar.Web/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace Sonar.Web.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "SEX", "TRUE" };
         }
 
         // GET api/values/5
