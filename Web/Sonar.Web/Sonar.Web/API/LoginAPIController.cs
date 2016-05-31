29967746100181105d208652653679dae82a2a98
zarko brkic <mceternal105@hotmail.com>
2016-05-31 22:09:17 +0200

    Refactor EventVM

---
+++ Web/Sonar.Web/Sonar.Web/API/LoginAPIController.cs
@@ -0,0 +1,29 @@
+﻿using Sonar.Web.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sonar.Web.API
+{
+    public class LoginAPIController : ApiController
+    {
+        [HttpPost]
+        public bool login(string usr, string pwd)
+        {
+            //var context = new hackathon_shift_2016_testEntities();
+
+            //return context.Event.Select(e => new EventVM()
+            //{
+            //    Longitude = e.Longitude,
+            //    Latitude = e.Latitude,
+            //    Name = e.Name,
+            //    Description = e.Description,
+            //    AuthorName = e.Person.FirstName + " " + e.Person.LastName
+            //});
+            return false;
+        }
+    }
+}
