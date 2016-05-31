aefe920e594e66f93ed08ba6a3b36a3bfba1a1c4
zarko brkic <mceternal105@hotmail.com>
2016-05-31 22:46:04 +0200

    Login

--- Web/Sonar.Web/Sonar.Web/API/LoginAPIController.cs
+++ Web/Sonar.Web/Sonar.Web/API/LoginAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Sonar.Web.Model;
+﻿using Sonar.ViewModels;
+using Sonar.Web.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,19 +11,28 @@ namespace Sonar.Web.API
 {
     public class LoginAPIController : ApiController
     {
+        private readonly hackathon_shift_2016_testEntities context;
+        public LoginAPIController()
+        {
+            context = new hackathon_shift_2016_testEntities();
+        }
+
         [HttpPost]
         public bool login(string usr, string pwd)
         {
-            //var context = new hackathon_shift_2016_testEntities();
+            var useri = context.Event.Select(e => new PersonVM()
+            {
+                Username = e.Person.Username,
+                Password = e.Person.Password
+            }).ToList();
 
-            //return context.Event.Select(e => new EventVM()
-            //{
-            //    Longitude = e.Longitude,
-            //    Latitude = e.Latitude,
-            //    Name = e.Name,
-            //    Description = e.Description,
-            //    AuthorName = e.Person.FirstName + " " + e.Person.LastName
-            //});
+            foreach(var user in useri)
+            {
+                if(user.Username == usr && user.Password == pwd)
+                {
+                    return true;
+                }
+            }
             return false;
         }
     }
