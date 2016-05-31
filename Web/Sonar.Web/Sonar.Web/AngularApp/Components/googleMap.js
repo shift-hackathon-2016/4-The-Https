c2f3b8a48143a4a02ce307d9dc4c2a2171239781
Mario <brizic.mario@gmail.com>
2016-05-31 19:47:01 +0200

    Added center on current user location

--- Web/Sonar.Web/Sonar.Web/AngularApp/Components/googleMap.js
+++ Web/Sonar.Web/Sonar.Web/AngularApp/Components/googleMap.js
@@ -1,6 +1,7 @@
 ﻿var apiKey = "AIzaSyDFN-6n_8rTK7x2sgRmONkTd7STu3yJpew";
 var googleMap = null;
 var currentMarkers = [];
+var DEFAULT_ZOOM = 13;
 
 mapDirective.$inject = [];
 function mapDirective() {
@@ -45,11 +46,12 @@ function addMarkerToMap(marker) {
 
 function mapLink(scope, element, attrs) {
     var element = document.getElementById('googleMap');
-    var currentLocation = getCurrentLocation();
+
 	googleMap = new google.maps.Map(element, {
-		zoom: 9,
-		center: currentLocation
+		zoom: DEFAULT_ZOOM
 	});
+
+	getCurrentLocationAndCenter();
 
 	googleMap.addListener("click", function (event) {
 	    scope.visibilityOptions.isAddEventModalVisible = true;
@@ -57,13 +59,14 @@ function mapLink(scope, element, attrs) {
 	});
 }
 
-function getCurrentLocation() {
+function getCurrentLocationAndCenter() {
     navigator.geolocation.getCurrentPosition(function (position) {
         var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
         googleMap.setCenter(pos);
+        googleMap.setZoom(DEFAULT_ZOOM);
     }, function () {
         alert('geolocation disabled');
     });
