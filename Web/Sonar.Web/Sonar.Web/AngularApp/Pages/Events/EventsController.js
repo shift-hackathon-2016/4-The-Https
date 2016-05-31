896a24ce90833ce8dd8e9a7a63253f88622f5fd9
Ante Rota <antrot3@gmail.com>
2016-05-31 20:34:26 +0200

    dsad

--- Web/Sonar.Web/Sonar.Web/AngularApp/Pages/Events/EventsController.js
+++ Web/Sonar.Web/Sonar.Web/AngularApp/Pages/Events/EventsController.js
@@ -1,6 +1,9 @@
 ﻿EventsController.$inject = ["$scope", "$http"];
 function EventsController($scope, $http) {
-
+    $http.get('api/Eventsapi/GetAllEventsForUser').then(function (response) {
+        $scope.events = response.data;
+        console.log(response);
+    });
 }
 
 sonar.controller('eventsController', EventsController);
