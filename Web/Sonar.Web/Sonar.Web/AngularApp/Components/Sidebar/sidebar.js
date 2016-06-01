8244adbc03f77456ce3c9cf477d95387f165db64
Ante Rota <antrot3@gmail.com>
2016-06-01 06:35:59 +0200

    sdasd

--- Web/Sonar.Web/Sonar.Web/AngularApp/Components/Sidebar/sidebar.js
+++ Web/Sonar.Web/Sonar.Web/AngularApp/Components/Sidebar/sidebar.js
@@ -6,12 +6,20 @@
     }
 }
 
-sidebarController.$inject = ["$scope", "$http"];
-function sidebarController($scope, $http) {
-    $http.get('api/ProfileApi/Get/').then(function (response) {
-        $scope.profile = response.data;
-        console.log($scope.profile);
+sidebarController.$inject = ["$rootScope","$scope", "$http"];
+function sidebarController($rootScope,$scope, $http) {
+    $http.get('api/friendsapi/GetAllFriendsForUser').then(function (response) {
+        $scope.friends = response.data;
     });
+    $http.get('api/eventsApi/GetAllEventsForUser').then(function (response) {
+        $scope.events = response.data;
+    });
+    $http.get('Api/ProfileApi/Get/', { params: { id: userId } }).then(function (response) {
+        $scope.profileViewModel = response.data;
+        setOtherFields();
+    });
+
+    
 }
 
 sonar.directive('sidebar', sidebar);
