f04317ff15c3ccf8f87d416ca7868d1ccb0b991f
Mario <brizic.mario@gmail.com>
2016-06-01 08:36:40 +0200

    Merge conflicts

--- Web/Sonar.Web/Sonar.Web/AngularApp/Components/Sidebar/sidebar.js
+++ Web/Sonar.Web/Sonar.Web/AngularApp/Components/Sidebar/sidebar.js
@@ -8,13 +8,9 @@
 
 sidebarController.$inject = ["$rootScope","$scope", "$http"];
 function sidebarController($rootScope,$scope, $http) {
-    $http.get('api/userApi/GetSidebarData/').then(function (response) {
+    $http.get('api/userApi/GetSidebarData/').then(function(response) {
         $scope.vm = response.data;
-    
-   
-    };
-
-    
-
+    });
+};
 
 sonar.directive('sidebar', sidebar);
