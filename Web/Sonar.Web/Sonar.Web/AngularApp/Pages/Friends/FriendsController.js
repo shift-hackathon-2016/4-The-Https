8de699e14697b487d3f4b9ed7bb370769c36cee7
Matija Čvrk <matijacvrk@gmail.com>
2016-06-01 06:32:45 +0200

    friends (people user is subscribed to) list

--- Web/Sonar.Web/Sonar.Web/AngularApp/Pages/Friends/FriendsController.js
+++ Web/Sonar.Web/Sonar.Web/AngularApp/Pages/Friends/FriendsController.js
@@ -1,6 +1,10 @@
 ﻿FriendsController.$inject = ["$scope", "$http"];
 function FriendsController($scope, $http) {
-    $http.get('api/friendsapi/GetAllFriendsForUser').then(function (response) {
+    $http.get('api/friendsapi/GetAllPeople').then(function (response) {
+        $scope.friends = response.data;
+    });
+
+    $http.get('api/friendsapi/GetAllFriendsForUser', localStorage.getItem('username')).then(function (response) {
         $scope.friends = response.data;
     });
 }
